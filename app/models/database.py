89e045750cd755b20c93ab7cc7d555bb851674bc
adamwhite-dev <31892823+adamwhite-dev@users.noreply.github.com>
2023-11-19 18:07:22 -0500

    Added create_database_if_not_exists()

--- app/models/database.py
+++ app/models/database.py
@@ -5,3 +5,10 @@ def get_database():
     # Placeholder for database connection
     client = MongoClient("mongodb://localhost:27017/")
     return client['video_games']
+
+
+def create_database_if_not_exists():
+    client = MongoClient("mongodb://localhost:27017/")
+    if 'video_games' not in client.list_database_names():
+        print("Creating 'video_games' database...")
+        client['video_games']  # This line accesses the database, effectively creating it
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from app.models.database import create_database_if_not_exists
 from app.views.game_view_controller import main as run_console_application
 
 
 def main():
+    create_database_if_not_exists()  # Ensure the database exists
     run_console_application()
 
 
