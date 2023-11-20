5b7e92c73a2c8fb9f3dd5bc4b6bac7642ac5abf0
Manzi ISHIMWE Patrick Yves <manz0023@algonquinlive.com>
2023-11-19 20:58:50 -0500

    updated test_database.py

--- tests/models/test_database.py
+++ tests/models/test_database.py
@@ -1,13 +1,14 @@
 import pytest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from app.models.database import get_database, create_database_if_not_exists
+from pymongo import MongoClient
 
 def test_get_database():
     # Mock MongoClient to avoid actual database connection
-    with pytest.raises(Exception):  # Ensure an exception is raised due to mock
-        get_database()
+    with patch("app.models.database.MongoClient") as mongo_mock:
+        # Raise an exception when the instance is called
+        mongo_mock.side_effect = Exception("Mocked exception")
+        with pytest.raises(Exception, match="Mocked exception"):
+            get_database()
 
-def test_create_database_if_not_exists():
-    # Mock MongoClient to avoid actual database connection
-    with pytest.raises(Exception):  # Ensure an exception is raised due to mock
-        create_database_if_not_exists()
+
